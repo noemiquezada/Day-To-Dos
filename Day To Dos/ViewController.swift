835ab9e266e028baa2ab51402de6691537146ec5
Noemi Quezada <noemi01@csu.fullerton.edu>
2016-09-21 22:34:19 -0700

    Initial Commit

---
+++ Day To Dos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day To Dos
+//
+//  Created by Noemi Quezada on 9/21/16.
+//  Copyright © 2016 Noemi Quezada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
