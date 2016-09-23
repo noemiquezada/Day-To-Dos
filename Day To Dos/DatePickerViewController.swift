2e4c9ca0023998589c7396a8ecac40ad434c3ae0
Noemi Quezada <noemi01@csu.fullerton.edu>
2016-09-22 22:12:45 -0700

    Created DatePicker View controller and added it to the storyboard as well as the to do list

---
+++ Day To Dos/DatePickerViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DatePickerViewController.swift
+//  Day To Dos
+//
+//  Created by Noemi Quezada on 9/22/16.
+//  Copyright © 2016 Noemi Quezada. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func retrieveList(_ sender: AnyObject) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
